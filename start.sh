0693a436b9e86f5488a41cbec676518b4f5758ac
xuezb <xuezb@glsc.com.cn>
2022-05-26 13:05:36 +0800

    测试HandlerMethodArgumentResolver,拦截Request请求

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+pid=`netstat -tlunp | grep 9999`
+if [ "$pid" != "" ];then 
+kill -9 `netstat -tlunp | grep 9999 | awk '{print $7}' | awk -F "/" '{print $1}'`
+fi 
 java -jar ./target/demo-0.0.1-SNAPSHOT.jar >> demo.log 2>&1 &
