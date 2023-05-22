fc13ed92125b967cf3dc356ffd68b68c23fd8350
xuezb <xuezb@glsc.com.cn>
2023-05-22 17:01:18 +0800

    commit

--- src/main/java/com/example/demo/config/WordsConfig.java
+++ src/main/java/com/example/demo/config/WordsConfig.java
@@ -5,10 +5,9 @@ import lombok.Data;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Configuration
@@ -16,16 +15,8 @@ public class WordsConfig {
 
     @Bean
     public Words getWords() throws Exception {
-        List<String> list = new ArrayList<>();
-        String filePath = "src/main/resources/StopWordsChinese.txt";
-        FileInputStream fin = new FileInputStream(filePath);
-        InputStreamReader reader = new InputStreamReader(fin);
-        BufferedReader buffReader = new BufferedReader(reader);
-        String strTmp = "";
-        while ((strTmp = buffReader.readLine()) != null) {
-            list.add(strTmp);
-        }
-        buffReader.close();
+        Path path = Paths.get("src/main/resources/StopWordsChinese.txt");
+        List<String> list = Files.readAllLines(path);
         return new Words(list);
     }
 
